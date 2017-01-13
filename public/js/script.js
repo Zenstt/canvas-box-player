1ae3f1d0e149394cea34f79c22ca81d4c5c7b9c1
Denis Cou <deniscou@gmail.com>
2017-01-13 16:06:43 +0100

    tick rate 2

--- public/js/script.js
+++ public/js/script.js
@@ -60,7 +60,7 @@ var myGameArea = {
         this.canvas.height = canvHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 30);
+        this.interval = setInterval(updateGameArea, 15);
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
