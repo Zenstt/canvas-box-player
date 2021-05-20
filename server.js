8a804a454bd8e92afd76c0c400009e48634beeda
Denis <deniscou@gmail.com>
2021-05-20 16:29:07 +0000

    Little changes, adapted to proxy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 "use strict";
-var express = require("express");
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const express = require("express");
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8003;
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
@@ -13,8 +13,8 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-var players = [];
-var id = 0;
+let players = [];
+let id = 0;
 app.post('/id', function(req,res){
 		id++;
 		res.send({
